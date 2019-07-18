14b85e595807408de1ffcd942bc05ed8756335a7
Pavel Goultiaev <pavel.goultiaev@gmail.com>
2019-07-18 13:39:13 +0000

    added config for vs code remote container

--- startDatabase.sh
+++ startDatabase.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
+# create a network to be able to work in a remote container in vscode
+# docker network create --driver bridge spring-testing
 
 docker stop demo-postgres
 docker rm demo-postgres
-docker run --name demo-postgres -p 15432:5432 -e POSTGRES_PASSWORD=password -e POSTGRES_USER=testuser -d postgres
+docker run --name demo-postgres --network spring-testing -p 15432:5432 -e POSTGRES_PASSWORD=password -e POSTGRES_USER=testuser -d postgres
